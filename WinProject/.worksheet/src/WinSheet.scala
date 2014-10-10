6940113112f31db7edca37b28123c67feaa1acd8
Tobias Alt <toalt@htwg-konstanz.de>
2014-10-10 16:59:14 +0200

    no1

---
+++ WinProject/.worksheet/src/WinSheet.scala
@@ -0,0 +1,4 @@
+object WinSheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(61); 
+  println("Welcome to the Scala worksheet");$skip(15); 
+  println(1+1)}
+}
---
+++ WinProject/src/WinSheet.sc
@@ -0,0 +1,4 @@
+object WinSheet {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  println(1+1)                                    //> 2
+}
